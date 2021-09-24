c3ab67018b142edb1b918eafc21cab12ce86accb
Kittanokas <kitai1012@gmail.com>
2021-09-24 21:49:59 +0900

    Issue 4

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,7 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
-    Plan.create(plan_params)
+    Plan.create(content: params[:plan_params])
     redirect_to action: :index
   end
 
